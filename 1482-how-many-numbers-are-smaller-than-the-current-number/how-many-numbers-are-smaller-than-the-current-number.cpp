5f3189482180e5903ab2f4fafdb0918b1325b8c7
Akhil Semwal <akhilsem.work@gmail.com>
2026-09-14 06:16:04 +0530

    Time: 0 ms (100.00%) | Memory: 14.2 MB (51.07%) - LeetSync

--- 1482-how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.cpp
+++ 1482-how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.cpp
@@ -1,17 +1,12 @@
-#include <vector>
-
 class Solution {
 public:
-    std::vector<int> smallerNumbersThanCurrent(std::vector<int>& nums) {
-        // Frequency array for values 0 to 100 based on problem constraints
+    vector<int> smallerNumbersThanCurrent(std::vector<int>& nums) {
+    
         int count[101] = {0};
 
         for (int num : nums) {
             count[num]++;
         }
-
-        // Transform into prefix sums where count[v] becomes the count
-        // of elements strictly smaller than v
         int running_sum = 0;
         for (int v = 0; v <= 100; v++) {
             int temp = count[v];
@@ -19,7 +14,7 @@ public:
             running_sum += temp;
         }
 
-        std::vector<int> result(nums.size());
+        vector<int> result(nums.size());
         for (int i = 0; i < nums.size(); i++) {
             result[i] = count[nums[i]];
         }
